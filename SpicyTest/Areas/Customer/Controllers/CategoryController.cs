cb312332ac24065ab9e67695cb835239cf09ac37
general44444 <aabd55389@gmail.com>
2021-03-18 11:16:16 -0700

    Add project files.

---
+++ SpicyTest/Areas/Customer/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpicyTest.Data;
+using SpicyTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpicyTest.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await db.Categories.ToListAsync();
+            return  View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+
+        }
+
+    }
+}
